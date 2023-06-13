4b97017c0ba89ffa7f21e254aa3961b308c89ae9
Daniel Goldacker <daniel-goldacker@hotmail.com>
2023-06-13 08:22:41 -0300

    Program altered for collect statistic infos

--- src/manager.py
+++ src/manager.py
@@ -2,6 +2,17 @@ from config import ConfigFiles
 from swagger import Swagger
 
 with open(ConfigFiles.PATH_FILE, "w") as arquivo:
-    arquivo.writelines("Infos of Swagger file:" + '\n')
+    arquivo.writelines("Statistics for Swagger file:" + '\n')
+    statisticsSwagger = Swagger.collectDataStatisticsSwaggerByMethod(ConfigFiles.SWAGGER_FILE)
+    totalMethodEndpoint = 0
+    for method, count in statisticsSwagger.items():
+        totalMethodEndpoint += count
+        arquivo.writelines(f" * {method}: {count}" + '\n')
+
+    arquivo.writelines('\n')
+    arquivo.writelines("Paths for Swagger file:" + '\n')
     for infosSwagger in Swagger.extractInfosFromSwagger(ConfigFiles.SWAGGER_FILE):
         arquivo.writelines(' * [' + infosSwagger[1].upper() + '] ' + infosSwagger[0] + '\n')
+
+    arquivo.writelines('\n')
+    arquivo.writelines("Total Endpoints: " + str(totalMethodEndpoint) + '\n')
--- src/swagger.py
+++ src/swagger.py
@@ -15,3 +15,28 @@ class Swagger:
                 return infosExtractSwagger
             except yaml.YAMLError as e:
                 print("Error loading swagger file:", e)
+
+
+
+    def collectDataStatisticsSwaggerByMethod(arquivo_swagger):
+        with open(arquivo_swagger, 'r') as arquivo:
+            try:
+                swaggerData = yaml.safe_load(arquivo)
+
+                statistics = {
+                    'GET': 0,
+                    'POST': 0,
+                    'PUT': 0,
+                    'PATCH': 0,
+                    'DELETE': 0
+                }
+
+                for path, pathDetails in swaggerData.get('paths', {}).items():
+                    for method in pathDetails.keys():
+                        methodUpper = method.upper()
+                        if methodUpper in statistics:
+                            statistics[methodUpper] += 1
+
+                return statistics
+            except yaml.YAMLError as e:
+                print("Error loading swagger file:", e)
